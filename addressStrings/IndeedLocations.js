2168979eccaa282b865e8c6029d8cf6490eb12c3
Stephen Hack <stephen.hack@gmail.com>
2018-11-05 22:04:34 -0500

    added glassdoor locations

---
+++ addressStrings/IndeedLocations.js
@@ -0,0 +1,206 @@
+exports.locations = {
+    _Average: {
+        indeedJobsString: '',
+        indeedSalariesString: ''
+    },
+    Atlanta: {
+        indeedJobsString: 'Atlanta,+GA',
+        indeedSalariesString: ',-Atlanta-GA'
+    },
+    Austin: {
+        indeedJobsString: 'Austin,+TX',
+        indeedSalariesString: ',-Austin-TX'
+    },
+    Baltimore: {
+        indeedJobsString: 'Baltimore,+MD',
+        indeedSalariesString: ',-Baltimore-MD'
+    },
+    Birmingham: {
+        indeedJobsString: 'Birmingham,+AL',
+        indeedSalariesString: ',-Birmingham-AL'
+    },
+    Boston: {
+        indeedJobsString: 'Boston,+MA',
+        indeedSalariesString: ',-Boston-MA'
+    },
+    Buffalo: {
+        indeedJobsString: 'Buffalo,+NY',
+        indeedSalariesString: ',-Buffalo-NY'
+    },
+    Charlotte: {
+        indeedJobsString: 'Charlotte,+NC',
+        indeedSalariesString: ',-Charlotte-NC'
+    },
+    Chicago: {
+        indeedJobsString: 'Chicago,+IL',
+        indeedSalariesString: ',-Chicago-IL'
+    },
+    Cincinnati: {
+        indeedJobsString: 'Cincinnati,+OH',
+        indeedSalariesString: ',-Cincinnati-OH'
+    },
+    Cleveland: {
+        indeedJobsString: 'Cleveland,+OH',
+        indeedSalariesString: ',-Cleveland-OH'
+    },
+    Columbus: {
+        indeedJobsString: 'Columbus,+OH',
+        indeedSalariesString: ',-Columbus-OH'
+    },
+    DC:  {
+        indeedJobsString: 'Washington,+DC',
+        indeedSalariesString: ',-Washington-DC'
+    },
+    Dallas:  {
+        indeedJobsString: 'Dallas,+TX',
+        indeedSalariesString: ',-Dallas-TX'
+    },
+    Denver: {
+        indeedJobsString: 'Denver,+CO',
+        indeedSalariesString: ',-Denver-CO'
+    },
+    Detroit: {
+        indeedJobsString: 'Detroit,+MI',
+        indeedSalariesString: ',-Detroit-MI'
+    },
+    Hartford: {
+        indeedJobsString: 'Hartford,+CT',
+        indeedSalariesString: ',-Hartford-CT'
+    },
+    Houston: {
+        indeedJobsString: 'Houston,+TX',
+        indeedSalariesString: ',-Houston-TX'
+    },
+    Indianapolis: {
+        indeedJobsString: 'Indianapolis,+IN',
+        indeedSalariesString: ',-Indianapolis-IN'
+    },
+    Jacksonville: {
+        indeedJobsString: 'Jacksonville,+FL',
+        indeedSalariesString: ',-Jacksonville-FL'
+    },
+    KC: {
+        indeedJobsString: 'Kansas+City,+MO',
+        indeedSalariesString: ',-Kansas-City-MO'
+    },
+    LA: {
+        indeedJobsString: 'Los+Angeles,+CA',
+        indeedSalariesString: ',-Los-Angeles-CA'
+    },
+    LasVegas: {
+        indeedJobsString: 'Las+Vegas,+NV',
+        indeedSalariesString: ',-Las-Vegas-NV'
+    },
+    Louisville: {
+        indeedJobsString: 'Louisville,+KY',
+        indeedSalariesString: ',-Louisville-KY'
+    },
+    Memphis: {
+        indeedJobsString: 'Memphis,+TN',
+        indeedSalariesString: ',-Memphis-TN'
+    },
+    Miami: {
+        indeedJobsString: 'Miami,+FL',
+        indeedSalariesString: ',-Miami-FL'
+    },
+    Milwaukee: {
+        indeedJobsString: 'Milwaukee,+WI',
+        indeedSalariesString: ',-Milwaukee-WI'
+    },
+    Minneapolis: {
+        indeedJobsString: 'Minneapolis,+MN',
+        indeedSalariesString: ',-Minneapolis-MN'
+    },
+    Nashville: {
+        indeedJobsString: 'Nashville,+TN',
+        indeedSalariesString: ',-Nashville-TN'
+    },
+    NewOrleans: {
+        indeedJobsString: 'New+Orleans,+LA',
+        indeedSalariesString: ',-New-Orleans-LA'
+    },
+    NYC: { 
+        indeedJobsString: 'New+York,+NY',
+        indeedSalariesString: ',-New-York-NY'
+    },
+    OklahomaCity: { 
+        indeedJobsString: 'Oklahoma+City,+OK',
+        indeedSalariesString: ',-Oklahoma-City-OK'
+    },
+    Orlando: { 
+        indeedJobsString: 'Orlando,+FL',
+        indeedSalariesString: ',-Orlando-FL'
+    },
+    Philly: { 
+        indeedJobsString: 'Philadelphia,+PA',
+        indeedSalariesString: ',-Philadelphia-PA'
+    },
+    Phoenix: { 
+        indeedJobsString: 'Phoenix,+AZ',
+        indeedSalariesString: ',-Phoenix-AZ'
+    },
+    Pittsburgh: { 
+        indeedJobsString: 'Pittsburgh,+PA',
+        indeedSalariesString: ',-Pittsburgh-PA'
+    },
+    Portland: { 
+        indeedJobsString: 'Portland,+OR',
+        indeedSalariesString: ',-Portland-OR'
+    },
+    Providence: { 
+        indeedJobsString: 'Providence,+RI',
+        indeedSalariesString: ',-Providence-RI'
+    },
+    Raleigh: { 
+        indeedJobsString: 'Raleigh,+NC',
+        indeedSalariesString: ',-Raleigh-NC'
+    },
+    Richmond: { 
+        indeedJobsString: 'Richmond,+VA',
+        indeedSalariesString: ',-Richmond-VA'
+    },
+    Riverside: { 
+        indeedJobsString: 'Riverside,+CA',
+        indeedSalariesString: ',-Riverside-CA'
+    },
+    SF: { 
+        indeedJobsString: 'San+Francisco,+CA',
+        indeedSalariesString: ',-San-Francisco-CA'
+    },
+    Sacramento: { 
+        indeedJobsString: 'Sacramento,+CA',
+        indeedSalariesString: ',-Sacramento-CA'
+    },
+    SaltLakeCity: { 
+        indeedJobsString: 'Salt+Lake+City,+UT',
+        indeedSalariesString: ',-Salt-Lake-City-UT'
+    },
+    SanAntonio: { 
+        indeedJobsString: 'San+Antonio,+TX',
+        indeedSalariesString: ',-San-Antonio-TX'
+    },
+    SanDiego: { 
+        indeedJobsString: 'San+Diego,+CA',
+        indeedSalariesString: ',-San-Diego-CA'
+    },
+    SanJose: { 
+        indeedJobsString: 'San+Jose,+CA',
+        indeedSalariesString: ',-San-Jose-CA'
+    },
+    Seattle: { 
+        indeedJobsString: 'Seattle,+WA',
+        indeedSalariesString: ',-Seattle-WA'
+    },
+    StLouis: { 
+        indeedJobsString: 'St.+Louis,+MO',
+        indeedSalariesString: ',-St.-Louis-MO'
+    },
+    Tampa: { 
+        indeedJobsString: 'Tampa,+FL',
+        indeedSalariesString: ',-Tampa-FL'
+    },
+    VirginiaBeach: { 
+        indeedJobsString: 'Virginia+Beach,+FL',
+        indeedSalariesString: ',-Virginia-Beach-FL'
+    },
+}
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const cheerio = require('cheerio');
 const mongoose = require('mongoose');
 const rp = require('request-promise');
 const Report = require('./models/report');
-const addressLocation = require('./addressStrings/locations');
-const addressJob = require('./addressStrings/jobs');
-const glassdoor = require('./addressStrings/glassdoor');
+const addressLocation = require('./addressStrings/IndeedLocations');
+const addressJob = require('./addressStrings/jobTitles');
+const glassdoorJobs = require('./addressStrings/glassdoorJobs')
+const glassdoorSalaries = require('./addressStrings/glassdoorSalaries');
 
 mongoose.connect("", { useNewUrlParser: true })
   .then(() => { 
@@ -17,121 +18,43 @@ mongoose.connect("", { useNewUrlParser: true })
   for (let job in addressJob.titles) {
     for (let location in addressLocation.locations) {
       timeout += 400;
-
-
       setTimeout(() => {
-        scrapeData(job, location).then((m) => console.log(m))
+        scrapeData(job, location).then((m) => {
+          console.log(m)
+        })
       }, timeout)
-
-      
-      
-      // .then(function(result) {
-      //   console.log(result)
-      //   const jobsIndeed = {
-      //     uri: `https://www.indeed.com/jobs?q=${result.title}&l=${result.locationString}&jt=fulltime`,
-      //     transform: function (body) {
-      //       return cheerio.load(body);
-      //     }
-      //   }
-      //   rp(jobsIndeed).then(($) => {
-      //     let totalJobs = $('#searchCount')
-      //       .text()
-      //       .slice(19, 30)
-      //       .split('')
-      //       .filter(character => character !== ',' && character < 10)
-      //       .join('')
-      //     return {
-      //       job: result.job,
-      //       jobsIndeed: totalJobs,
-      //       metroArea: result.metroArea,
-      //       salaryIndeed: result.salaryIndeed,
-      //       samplesIndeed: result.samplesIndeed
-      //     }
-      //   }).catch((err) => { console.log('Jobs not working')})
-      // })
-      // .then(function(result) {
-      //   console.log('hello')
-      // })
-      // .then(function(result) {
-      //   setTimeout(function() {
-      //     const glassdoorUrl = 'http://api.scraperapi.com/?key=522d8cfbb9b37afb76e391a3d0114b60&url=' + glassdoor.strings[result.job][result.metroArea];
-
-      //     console.log('resultjob', result.job)
-      //     console.log('resultMetro', result.metroArea)
-      //     console.log('glassdoorUrl', glassdoorUrl)
-        
-      //     const glassdoorSalary = {
-      //       uri: glassdoorUrl,
-      //       transform: function (body) {
-      //         return cheerio.load(body);
-      //       }
-      //     }
-      
-      //     rp(glassdoorSalary).then(($) => {
-      //       console.log($('.OccMedianBasePayStyle__payNumber').text())
-      //       let salary = Number($('.OccMedianBasePayStyle__payNumber').text()
-      //         .replace('$', '')
-      //         .replace(',', ''))
-
-      //       console.log(salary)
-            
-      //       let samples = Number($('.spacingHelpers__margRt').text()
-      //         .replace('SalariesEmployer nameAverage Base Salaries in (USD)', '')
-      //         .replace(',', ''))
-
-      //       return {
-      //         job: result.job,
-      //         jobsIndeed: result.jobsIndeed,
-      //         metroArea: result.metroArea,
-      //         salaryGlassdoor: salary,
-      //         salaryIndeed: result.salaryIndeed,
-      //         samplesGlassdoor: samples,
-      //         samplesIndeed: result.samplesIndeed
-      //       }
-      
-      //     }).catch((err) => { console.log(err, 'Jobs not working')})
-      //   }, 500)
-      // })
-      // .then(function(result) {
-      //   let today = new Date();
-      //   let day = today.getUTCDate();
-      //   let month = today.getMonth();
-      //   let year = today.getFullYear();
-        
-      //   const report = new Report({
-      //     created: today,
-      //     day: day,
-      //     glassdoorSalary: result.salaryGlassdoor,
-      //     indeedJobs: result.jobsIndeed,
-      //     indeedSalary: result.indeedSalary,
-      //     indeedSampleSet: result.samplesIndeed,
-      //     title: result.job,
-      //     metro: result.metroArea,
-      //     month: month,
-      //     year: year
-      //   })
-      //   console.log(report)
-      //   report.save(function (err, sucess) {
-      //     if (err) console.log(err)
-      //   })
-      // })
     }
   }
 }).then(function(result) {
   setTimeout(function() {
     console.log('Mongo Connection Closed');
     mongoose.connection.close()
-  }, 20000)
+  }, 30000)
 })
 
 async function scrapeData(job, location) {
-  let indeedJobs = addressLocation.locations[location].indeedJobsString
-  let indeedSalaries = addressLocation.locations[location].indeedSalariesString
-  let jobTitle = addressJob.titles[job]
+  const key = 'http://api.scraperapi.com/?key=&url='
 
-  const getGlassdoorData = new Promise((resolve, reject) => {
-    let key = ''
-    const glassdoorUrl = key  + glassdoor.strings[result.job][result.metroArea];
+  const getGlassdoorJobsData = new Promise((resolve, reject) => {
+    const glassdoorUrl = key + glassdoorJobs.strings[job][location];
+    const glassdoorJob = {
+      uri: glassdoorUrl,
+      transform: function (body) {
+        return cheerio.load(body);
+      }
+    }
+
+    rp(glassdoorJob).then(($) => {
+      let jobs = Number($('.jobsCount').text()
+        .replace(',', '')
+        .replace('Jobs', ''))
+
+      resolve(jobs)
+    }).catch((err) => { console.log('Glassdoor Jobs Not working for ', job, location)})
+  })
+
+  const getGlassdoorSalaryData = new Promise((resolve, reject) => {
+    const glassdoorUrl = key  + glassdoorSalaries.strings[job][location];
     const glassdoorSalary = {
       uri: glassdoorUrl,
       transform: function (body) {
@@ -152,44 +75,90 @@ async function scrapeData(job, location) {
         salary: salary,
         samples: samples
       })
-    })
+    }).catch((err) => { console.log('Glassdoor Salary Not working for ', job, location)})
   })
 
-  let test = await getGlassdoorData;
-
-  return test;
-}
-
-async function getIndeedData(jobTitle, indeedSalaries) {
-  const salaries = {
-    uri: `https://www.indeed.com/salaries/${jobTitle}-Salaries${indeedSalaries}`,
-    transform: function (body) {
-      return cheerio.load(body);
+  const getIndeedJobData = new Promise((resolve, reject) => {
+    let jobsIndeed
+    if (location !== '_Average'){
+      jobsIndeed = {
+        uri: `https://www.indeed.com/jobs?q=${job}&l=${location}&jt=fulltime`,
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      }
+    } else if (location === '_Average') {
+      jobsIndeed = {
+        uri: `https://www.indeed.com/jobs?q=${job}&jt=fulltime`,
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      }
     }
+    
+    rp(jobsIndeed).then(($) => {
+      let totalJobs = $('#searchCount')
+        .text()
+        .slice(19, 30)
+        .split('')
+        .filter(character => character !== ',' && character < 10)
+        .join('')
+      resolve(totalJobs)
+    }).catch((err) => { console.log('Jobs not working for', job, location)})
+  })
+
+  const getIndeedSalaryData = new Promise((resolve, reject) => {
+    let indeedSalaries = addressLocation.locations[location].indeedSalariesString
+    const salaries = {
+      uri: `https://www.indeed.com/salaries/${addressJob.titles[job]}-Salaries${indeedSalaries}`,
+      transform: function (body) {
+        return cheerio.load(body);
+      }
+    }
+    rp(salaries).then(function ($) {
+      let salary = Number($('.cmp-sal-salary .cmp-salary-amount')
+        .text()
+        .split('').filter(character => character !== '$' && character !== ',')
+        .join(''))
+      if (salary === 0) {
+        console.log($.text())
+      } else if (salary < 1000) {
+        salary = salary * 2087
+      }
+      let samples = $('.cmp-salary-header-content')
+        .text().split('')
+        .slice(0, 30)
+        .filter(character => character < 10 && character !== ' ' )
+        .join('')
+      
+      resolve({
+        salary: salary,
+        samples: samples
+      })
+    }).catch((err) => { console.log('Indeed Salaries not working for ', job, location)})
+  })
+
+  let glassdoorSalaryData = await getGlassdoorSalaryData;
+  let indeedSalaryData = await getIndeedSalaryData;
+  let indeedJobData = await getIndeedJobData;
+  let glassdoorJobsData = await getGlassdoorJobsData;
+
+  let now = new Date();
+  let day = now.getUTCDate()
+  let month = now.getMonth()
+  let year = now.getFullYear()
+ 
+  return {
+    day: day,
+    glassdoorJobs: glassdoorJobsData,
+    glassdoorSalary: glassdoorSalaryData.salary,
+    glassdoorSalarySamples: glassdoorSalaryData.samples,
+    indeedJobs: indeedJobData,
+    indeedSalary: indeedSalaryData.salary,
+    indeedSalarySamples: indeedSalaryData.samples,
+    job: job,
+    location: location,
+    month: month,
+    year: year
   }
-  rp(salaries).then(function ($) {
-    let salaryIndeed = Number($('.cmp-sal-salary .cmp-salary-amount')
-      .text()
-      .split('').filter(character => character !== '$' && character !== ',')
-      .join(''))
-    if (salaryIndeed === 0) {
-      console.log($.text())
-    } else if (salaryIndeed < 1000) {
-      salaryIndeed = salaryIndeed * 2087
-    }
-    let samplesIndeed = $('.cmp-salary-header-content')
-      .text().split('')
-      .slice(0, 30)
-      .filter(character => character < 10 && character !== ' ' )
-      .join('')
-    return { 
-      job: job,
-      metroArea: location,
-      locationString: indeedJobs,
-      title: jobTitle,
-      salaryIndeed: salaryIndeed,
-      samplesIndeed: samplesIndeed,
-    }
-  }).catch(function (err) { console.log("Salaries not working")})
 }
-   
